8e2212aa44175ade07d7fd8f0301da1be627fb7c
EmielMatthys <emielmatthys@hotmail.com>
2021-05-02 18:35:46 +0200

    Add getvfunc closes #7

---
+++ getvfunc.h
@@ -0,0 +1,27 @@
+//
+// Created by emiel on 2/05/21.
+//
+
+#ifndef RESPECTRAL_GETVFUNC_H
+#define RESPECTRAL_GETVFUNC_H
+
+//Credits: Casual_Hacker
+
+#include <cstddef>
+
+inline void**& getvtable(void* inst, size_t offset = 0 )
+{
+    return *reinterpret_cast<void***>( (size_t)inst + offset );
+}
+inline const void** getvtable( const void* inst, size_t offset = 0 )
+{
+    return *reinterpret_cast<const void***>( (size_t)inst + offset );
+}
+template< typename Fn >
+inline Fn getvfunc( const void* inst, size_t index, size_t offset = 0 )
+{
+    return reinterpret_cast<Fn>( getvtable( inst, offset )[ index ] );
+}
+//===================================================================================
+
+#endif //RESPECTRAL_GETVFUNC_H
