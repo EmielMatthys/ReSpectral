5bbce90147db1926dee558581d04f07dd15fe006
EmielMatthys <emielmatthys@hotmail.com>
2021-05-15 01:07:58 +0200

    Add inject scripts

---
+++ scripts/load.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+param1=$1
+param2=$2
+
+echo param 1: $param1
+echo param 2: $param2
+
+line=$(pidof $1)
+arr=($line)
+inst=0
+
+if [ ${#arr[@]} == 0 ]; then
+  echo Process isn\'t running!
+  exit
+fi
+
+if [ $inst -gt ${#arr[@]} ] || [ $inst == ${#arr[@]} ]; then
+  echo wrong index!
+  exit
+fi
+
+if ![ -f "$2" ]; then
+  echo $2 file does not exist!
+  exit
+fi
+
+proc=${arr[$inst]}
+
+echo Running instances: "${arr[@]}"
+echo Attaching to "$proc"
+
+gdb -n -q -batch \
+  -ex "attach $proc" \
+  -ex "set \$dlopen = (void*(*)(char*, int)) dlopen" \
+  -ex "call \$dlopen(\"$2\", 1)" \
+  -ex "call (char*) dlerror()" \
+  -ex "detach" \
+  -ex "quit"
+
+# set $dlopen = (void*(*)(char*, int)) dlopen
+# call $dlopen("/home/emiel/Documents/cpp/SpectralHook/build/libSpectralHook.so", 1)
---
+++ scripts/unload.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+line=$(pidof $1)
+arr=($line)
+inst=0
+
+if [ ${#arr[@]} == 0 ]; then
+  echo Process isn\'t running!
+  exit
+fi
+
+if [ $inst -gt ${#arr[@]} ] || [ $inst == ${#arr[@]} ]; then
+  echo wrong index!
+  exit
+fi
+
+if ![ -f "$2" ]; then
+  echo $2 file does not exist!
+  exit
+fi
+
+proc=${arr[$inst]}
+
+echo Running instances: "${arr[@]}"
+echo Attaching to "$proc"
+
+gdb -n -q -batch \
+  -ex "attach $proc" \
+  -ex "set \$dlopen = (void*(*)(char*, int)) dlopen" \
+  -ex "set \$dlclose = (int(*)(void*)) dlclose" \
+  -ex "set \$lib = \$dlopen(\"$2\", 6)" \
+  -ex "call \$dlclose(\$lib)" \
+  -ex "call \$dlclose(\$lib)" \
+  -ex "call (char*) dlerror()" \
+  -ex "detach" \
+  -ex "quit" 
